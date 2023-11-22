7a78ecd27510d6dd287e2cbafacc58ca8ecef9a6
Aman Gusain <aman.gusain@annalect.com>
2023-11-22 11:09:44 +0530

    feat: added edit option in table

--- app/components/product-form.js
+++ app/components/product-form.js
@@ -3,14 +3,32 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class ProductFormComponent extends Component {
+  @tracked productId = null;
   @tracked product;
   @tracked category = null;
+  @tracked lastOrderDate = null;
   @tracked brand;
   @tracked stock;
   @tracked price;
   @tracked supplier;
   @tracked rating = 0;
-
+  @tracked editProduct = false;
+  constructor() {
+    super(...arguments);
+    if (this.args.product) {
+      const currentProductData = this.args.product;
+      this.product = currentProductData.product;
+      this.category = currentProductData.category;
+      this.productId = currentProductData.id;
+      this.brand = currentProductData.brand;
+      this.stock = currentProductData.stock;
+      this.price = currentProductData.price;
+      this.supplier = currentProductData.supplier;
+      this.rating = currentProductData.rating;
+      this.lastOrderDate = currentProductData.lastOrderDate;
+      this.editProduct = true;
+    }
+  }
   @action
   submitForm(e) {
     e.preventDefault();
@@ -22,6 +40,8 @@ export default class ProductFormComponent extends Component {
       price: this.price,
       supplier: this.supplier,
       rating: this.rating,
+      productId: this.productId,
+      lastOrderDate: this.lastOrderDate,
     });
   }
 
--- app/controllers/inventory.js
+++ app/controllers/inventory.js
@@ -9,6 +9,7 @@ export default class InventoryController extends Controller {
   @tracked data;
   @tracked products;
   @tracked dataPerPage;
+  @tracked currentProduct = null;
 
   constructor() {
     super(...arguments);
@@ -24,9 +25,10 @@ export default class InventoryController extends Controller {
   @action
   closeModal() {
     this.showModal = false;
+    this.currentProduct = null;
   }
   @action
-  async addProduct(product) {
+  async addNewProduct(product) {
     const url = 'http://127.0.0.1:3000/products';
     const data = product;
 
@@ -53,5 +55,52 @@ export default class InventoryController extends Controller {
       });
     this.products = [...this.products, newProduct];
     this.showModal = false;
+  }
+  @action
+  async editProduct(product) {
+    const url = `http://127.0.0.1:3000/products/${product.productId}`;
+    const data = product;
+
+    const fetchOptions = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+    const editedProduct = await fetch(url, fetchOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.error('Error during POST request:', error);
+      });
+    const index = this.products.findIndex(
+      (item) => item.id === editedProduct.productId,
+    );
+    const products = this.products;
+    products[index] = editedProduct;
+    this.products = [...products];
+    this.showModal = false;
+  }
+  @action
+  async addProduct(product) {
+    if (product.productId) {
+      this.editProduct(product);
+    } else {
+      this.addNewProduct(product);
+    }
+  }
+
+  @action
+  setCurrentProduct(product) {
+    this.showModal = true;
+    this.currentProduct = product;
   }
 }
